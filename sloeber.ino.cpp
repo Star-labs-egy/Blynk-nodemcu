37371379e4e5d1254207fa70543956c5890e7cf0
mohammed.nabil <mohammed.nabil@embeddedfab.com>
2018-04-03 23:17:00 +0200

    first commit and blynktemp arduino eclipse

---
+++ sloeber.ino.cpp
@@ -0,0 +1,20 @@
+#ifdef __IN_ECLIPSE__
+//This is a automatic generated file
+//Please do not modify this file
+//If you touch this file your change will be overwritten during the next build
+//This file has been generated on 2018-04-03 23:12:39
+
+#include "Arduino.h"
+#define USE_SPARKFUN_BLYNK_BOARD    // Uncomment the board you are using
+#define APP_DEBUG        // Comment this out to disable debug prints
+#define BLYNK_PRINT Serial
+#include <BlynkSimpleEsp8266.h>
+#include "BlynkProvisioning.h"
+
+void setup() ;
+void loop() ;
+
+
+#include "Template_ESP8266.ino"
+
+#endif
