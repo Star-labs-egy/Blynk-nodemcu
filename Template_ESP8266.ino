41c2f893cc3d9fac458111db4ba8567ae105fa1a
mohammed.nabil <mohammed.nabil@embeddedfab.com>
2018-04-03 23:22:24 +0200

    switch to nodemcu

--- Template_ESP8266.ino
+++ Template_ESP8266.ino
@@ -9,8 +9,8 @@
 
  *************************************************************/
 
-#define USE_SPARKFUN_BLYNK_BOARD    // Uncomment the board you are using
-//#define USE_NODE_MCU_BOARD        // Comment out the boards you are not using
+//#define USE_SPARKFUN_BLYNK_BOARD    // Uncomment the board you are using
+#define USE_NODE_MCU_BOARD        // Comment out the boards you are not using
 //#define USE_WITTY_CLOUD_BOARD
 //#define USE_CUSTOM_BOARD          // For all other ESP8266-based boards -
                                     // see "Custom board configuration" in Settings.h
--- sloeber.ino.cpp
+++ sloeber.ino.cpp
@@ -2,10 +2,10 @@
 //This is a automatic generated file
 //Please do not modify this file
 //If you touch this file your change will be overwritten during the next build
-//This file has been generated on 2018-04-03 23:12:39
+//This file has been generated on 2018-04-03 23:21:39
 
 #include "Arduino.h"
-#define USE_SPARKFUN_BLYNK_BOARD    // Uncomment the board you are using
+#define USE_NODE_MCU_BOARD        // Comment out the boards you are not using
 #define APP_DEBUG        // Comment this out to disable debug prints
 #define BLYNK_PRINT Serial
 #include <BlynkSimpleEsp8266.h>
